ca8d427c9ec9aa485b1d688c0a45c8c96bf3b87f
Aditi Lath <aditilath14@gmail.com>
2020-07-07 20:14:09 +0530

    Testing push from travis

---
+++ upload_target.sh
@@ -0,0 +1,6 @@
+  git config --global user.email "aditilath5@gmail.com"
+  git config --global user.name "aditilath"
+  git add -f.
+  git commit -m "Travis build pushed"
+  git push
+  echo -e" Done \ n "
